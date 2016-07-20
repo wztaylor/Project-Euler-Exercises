9500583c4b6fb944ef9caecd11022b2d03a97a4b
Zac Taylor <wztaylor01@gmail.com>
2016-07-19 20:05:58 -0400

    Finished Smallest Multiple program
    Problem was to determine the smallest number evenly divisible by all of
    the numbers from 1 - 20. I started this problem months ago and just
    restarted work on it. I finished the problem however I need a better way
    to determine a good starting number for n.

--- SmallestMultiple.py
+++ SmallestMultiple.py
@@ -1,32 +1,27 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 10 23:27:17 2015
-
 @author: Zac
-"""
-## pretty sure answer is 116396280
+Problem 5: 
+2520 is the smallest number that can be divided by each 
+of the numbers from 1 to 10 without any remainder.
 
-def factor(n):              #gives a list of all the factors of n
-    factors = []
-    for i in range(2,n+1):
-        if n % i == 0:
-            factors.append(i)
-    return factors
+What is the smallest positive number that is 
+evenly divisible by all of the numbers from 1 to 20?
+"""
     
 def smallestMult():
-    
-    for i in range(2500):
-        
-        
-
-    for z in range(2,20):
-        
-    
-
-
-
-
-
+    isDivisible = False
+    n = 100000000    
+    while(isDivisible == False):         
+        for i in range(2,20):
+            if n % i == 0:
+                isDivisible = True
+            else:
+                isDivisible = False
+                break
+        n += 1
+    return n-1
 
 
     
